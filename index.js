43d727a63a5ea557779280ee5f6530679e6c638e
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-05-03 22:53:35 +0900

    2023.05.03_22:53

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+// 스크롤 효과
+$("html").easeScroll();
+$("html").easeScroll({
+  frameRate: 60,
+  animationTime: 6000,
+  stepSize: 120,
+  pulseAlgorithm: !0,
+  pulseScale: 8,
+  pulseNormalize: 1,
+  accelerationDelta: 20,
+  accelerationMax: 1,
+  keyboardSupport: !0,
+  arrowScroll: 10
+});
+
+// section01
