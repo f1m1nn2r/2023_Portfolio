a9c0dadf47dc2746c4f50636e3e964336b4ea165
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-05-06 16:30:06 +0900

    2023.05.06_16:30

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ $("html").easeScroll({
     arrowScroll: 10
 });
 
+// 헤더, 풋터 - 전체 스크롤 값이 섹션03의 (스크롤 값+500) 값과 같거나 커질 경우 노출 안 되도록
+const header = document.querySelector('header');
+const footer = document.querySelector('.footer-section');
+window.addEventListener('scroll', function(){
+    if(window.scrollY >= section03.offsetTop){
+        header.classList.add('no-exposure'); 
+        footer.classList.add('no-exposure');
+    }else{
+        header.classList.remove('no-exposure'); 
+        footer.classList.remove('no-exposure');
+    }
+});
 
 // 섹션01
 const section01 = document.querySelector('.section01');
@@ -25,10 +37,21 @@ const sc02_RLScroll_comm = document.querySelectorAll('.section02 .RL-comm');
 const sc02_RLScroll_comm_txt = document.querySelectorAll('.section02 .RL-comm h1'); 
 var heightSum = 0;
 for (var i = 0; i < sc02_RLScroll_comm.length; i++) { 
-    sc02_RLScroll_comm[i].style.height = sc02_RLScroll_comm_txt[i].clientHeight / 10 + 'vh'; 
+    sc02_RLScroll_comm[i].style.height = sc02_RLScroll_comm_txt[i].clientHeight / 10 + 'vh'; // RL-comm의 height 값이 h1의 높이값과 동일하도록
     heightSum += sc02_RLScroll_comm_txt[i].clientHeight;
 }
 section02.style.height = heightSum/10 + 'vh';
+window.addEventListener('scroll', function(){
+    // 섹션02 - 스크롤 시 텍스트 좌 우 이동
+    for(var i=0; i<sc02_RLScroll_comm.length; i++){
+        if(i % 2 == 0){
+            sc02_RLScroll_comm_txt[i].style.transform = `translate3d(-${window.scrollY / sc02_RLScroll_comm[i].clientHeight * 2}vw, 0, 0) translateY(-50%)`
+        }else if(i % 2 == 1){
+            sc02_RLScroll_comm_txt[i].style.transform = `translate3d(${window.scrollY / sc02_RLScroll_comm[i].clientHeight * 2}vw, 0, 0) translateY(-50%) scale(-1)`
+        }
+    }
+});
+
 // 섹션03
 const section03 = document.querySelector('.sticky-section');
 const fir_txt = document.querySelectorAll('.sc01-txt-wrap__1 p');
@@ -43,37 +66,17 @@ for(var i=0; i<fir_txt.length; i++){
 const section03_txt = document.querySelectorAll('.section03 .txt-wrap .txt-inner');
 const circle = document.querySelector('.float');
 const circle_exposure = document.querySelector('.line-circle-exposure');
-const section04 = document.querySelector('.section04');
 window.addEventListener('scroll', function(){
     var windowY = window.scrollY;
-    // 헤더, 풋터 - 섹션03 도달 시 노출 안 되도록
-    const header = document.querySelector('header');
-    const footer = document.querySelector('.footer-section');
-
-    // 섹션02 - 스크롤 시 텍스트 좌 우 이동
-    for(var i=0; i<sc02_RLScroll_comm.length; i++){
-        if(i % 2 == 0){
-            sc02_RLScroll_comm_txt[i].style.transform = `translate3d(-${window.scrollY / sc02_RLScroll_comm[i].clientHeight * 2}vw, 0, 0) translateY(-50%)`
-        }else if(i % 2 == 1){
-            sc02_RLScroll_comm_txt[i].style.transform = `translate3d(${window.scrollY / sc02_RLScroll_comm[i].clientHeight * 2}vw, 0, 0) translateY(-50%) scale(-1)`
-        }
-    }
-    
     var circleValue = (windowY / 100) - 16;
-    // 전체 스크롤 값이 섹션03의 (스크롤 값+500) 값과 같거나 커질 경우
     // +500은 섹션02 넘어갈 때 height 값을 100vh로 설정하는 sticky 클래스 때문인지 스크롤 동작이 제대로 안 돼서 추가함
     if(windowY >= section03.offsetTop + 500){
         circle.classList.add('sticky'); // sticky 클래스 추가
         circle_exposure.style.transform = `translate3d(${circleValue}vw, 0, 0)`; // circle 배경 라인 노출되도록
-        header.classList.add('no-exposure'); // 섹션03일 시 헤더 비노출
-        footer.classList.add('no-exposure'); // 섹션03일 시 풋터 비노출
         section03.classList.add('border'); // 섹션03일 시 좌 우 하단 border 추가
-    }//else if(windowY >= section04.offsetTop / 1.2){ // circle 섹션04 이전에 sticky 클래스 삭제 <<<<< 동작 안 되고 있음
-        //circle.classList.remove('sticky');}
-    else{
+    }else{
         circle.classList.remove('sticky');
-        header.classList.remove('no-exposure');
-        footer.classList.remove('no-exposure');
+        circle_exposure.style.transform = `translate3d(-${circleValue}vw, 0, 0)`;
         section03.classList.remove('border');
     }
 });
@@ -86,7 +89,7 @@ const sc05_RLScroll_comm = document.querySelectorAll('.section05 .RL-comm');
 const sc05_RLScroll_comm_txt = document.querySelectorAll('.section05 .RL-comm h1');
 var sc05_heightSum = 0;
 for(var i=0; i<sc05_RLScroll_comm.length; i++){
-    sc05_RLScroll_comm[i].style.height = sc05_RLScroll_comm_txt[i].clientHeight/10 + '%';
+    sc05_RLScroll_comm[i].style.height = sc05_RLScroll_comm_txt[i].clientHeight/10 + 'vh';
     sc05_heightSum += sc05_RLScroll_comm_txt[i].clientHeight;
 }
 section05.style.height = sc05_heightSum/10 + 'vh';
@@ -94,15 +97,15 @@ window.addEventListener('scroll', function(){
     for(var i=0; i<sc05_RLScroll_comm.length; i++){
         if(window.scrollY >= section05.offsetTop / 1.3){
             if(i % 2 == 0){
-                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(-${window.scrollY / section01.offsetTop -7 }vw, 0, 0) translateY(-50%)`
+                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(-${window.scrollY * 2 / sc05_RLScroll_comm_txt[i].clientHeight - 10 }vw, 0, 0) translateY(-50%)`
             }else if(i % 2 == 1){
-                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(${window.scrollY / section01.offsetTop - 7 }vw, 0, 0) translateY(-50%) scale(-1)`
+                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(${window.scrollY * 2 / sc05_RLScroll_comm_txt[i].clientHeight - 10 }vw, 0, 0) translateY(-50%) scale(-1)`
             }
         }else{
             if(i % 2 == 0){
-                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(0, 0, 0) translateY(-50%)`
+                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(${window.scrollY * 2 / sc05_RLScroll_comm_txt[i].clientHeight }vw, 0, 0) translateY(-50%)`
             }else if(i % 2 == 1){
-                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(0, 0, 0) translateY(-50%) scale(-1)`
+                sc05_RLScroll_comm_txt[i].style.transform = `translate3d(-${window.scrollY * 2 / sc05_RLScroll_comm_txt[i].clientHeight}vw, 0, 0) translateY(-50%) scale(-1)`
             }
         }
     }
