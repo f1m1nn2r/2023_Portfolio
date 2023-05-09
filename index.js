49492158c2f883d94c404fce434e7b620006963a
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-05-09 17:16:36 +0900

    2023.05.09_17:16

--- index.js
+++ index.js
@@ -15,17 +15,16 @@ $("html").easeScroll({
     arrowScroll: 10
 });
 
-
 // 섹션02
 const section02 = document.querySelector('.section02'); 
 const sc02_RLScroll_comm = document.querySelectorAll('.section02 .RL-comm'); 
 const sc02_RLScroll_comm_txt = document.querySelectorAll('.section02 .RL-comm h1'); 
 var heightSum = 0;
 for (var i = 0; i < sc02_RLScroll_comm.length; i++) { 
-    sc02_RLScroll_comm[i].style.height = sc02_RLScroll_comm_txt[i].scrollHeight / 10 + 'vh'; // RL-comm의 height 값이 h1의 높이값과 동일하도록
+    sc02_RLScroll_comm[i].style.height = sc02_RLScroll_comm_txt[i].scrollHeight + 'px'; // RL-comm의 height 값이 h1의 높이값과 동일하도록
     heightSum += sc02_RLScroll_comm_txt[i].scrollHeight;
 }
-section02.style.height = heightSum/10 + 'vh';
+section02.style.height = heightSum + 'px';
 
 // 섹션03
 const section03 = document.querySelector('.sticky-section');
@@ -37,9 +36,13 @@ for(var i=0; i<fir_txt.length; i++){
     widthSum += fir_txt_width;
     fir_line.style.height = `calc(100% - ${widthSum}px)`;
 }
-const section03_txt = document.querySelectorAll('.section03 .txt-wrap .txt-inner');
-const circle = document.querySelector('.float');
-const circle_exposure = document.querySelector('.line-circle-exposure');
+const section03_txt = document.querySelectorAll('.section03 .txt-wrap');
+var sc03HeightSum = 0;
+for(var i=0; i<section03_txt.length; i++){
+    var section03_txt_height = section03_txt[i].scrollHeight;
+    sc03HeightSum += section03_txt_height;
+}
+section03.style.height = sc03HeightSum + 'px';
 
 // 섹션05
 const section05 = document.querySelector('.section05');
@@ -47,29 +50,54 @@ const sc05_RLScroll_comm = document.querySelectorAll('.section05 .RL-comm');
 const sc05_RLScroll_comm_txt = document.querySelectorAll('.section05 .RL-comm h1');
 var sc05_heightSum = 0;
 for(var i=0; i<sc05_RLScroll_comm.length; i++){
-    sc05_RLScroll_comm[i].style.height = sc05_RLScroll_comm_txt[i].scrollHeight/10 + 'vh';
+    sc05_RLScroll_comm[i].style.height = sc05_RLScroll_comm_txt[i].scrollHeight + 'px';
     sc05_heightSum += sc05_RLScroll_comm_txt[i].scrollHeight;
 }
-section05.style.height = sc05_heightSum/10 + 'vh';
+section05.style.height = sc05_heightSum + 'px';
 
 // 섹션06
-
 const workSection = document.querySelector('.work-section');
 const workSectionWrap = document.querySelector('.work-img-wrap');
 for(var i=1; i<11; i++){
     const workDiv = document.createElement('div'); // div 태그 생성
-    workDiv.setAttribute('class', 'work-img-slide'); // 해당 div 클래스명은 work-img-slide
-    //workSectionWrap.appendChild(workDiv); // work-img-wrap 태그 안에 work-img-slide 삽입
+    workDiv.setAttribute('class', 'work-img-slide');
+    workSectionWrap.appendChild(workDiv); // work-img-wrap 태그 안에 work-img-slide 삽입
 }
 const workImgSlide = document.querySelectorAll('.work-img-slide');
-const workImg = document.createElement('img');
 var workImgArray = new Array();
 for(var i=0; i<workImgSlide.length; i++){
     workImgArray[i] = new Image();
     workImgArray[i].src = `./img/work-img-0${i + 1}.png`;
-    workImg.src = workImgArray[i].src;
-
     workImgSlide[i].append(workImgArray[i]);
+}
+
+// 섹션07
+const section07 = document.querySelector('.section07');
+const section07_inner = document.querySelector('.section07-inner');
+const section07_child = document.querySelectorAll('.section07-inner > div');
+var childWidthSum = 0;
+for(var i=0; i<section07_child.length; i++){
+    childWidthSum += section07_child[i].scrollWidth;
+}
+section07_inner.style.width = childWidthSum + 'px';
+
+// 윈도우 로드 완료되면
+const section01 = document.querySelector('.section01');
+const introText = document.querySelectorAll('.section01 .sc01_comm h1');
+const introTextSpan = document.querySelectorAll('.section01 .sc01_comm h1 span');
+const header = document.querySelector('.at-header');
+const footer = document.querySelector('.footer-section');
+window.onload = function(){
+    section01.classList.add('load_ani');
+    header.classList.add('load_ani');
+    footer.classList.add('load_ani');
+    section02.classList.add('load_ani');
+    setTimeout(function(){
+        document.body.classList.remove('no-scroll');
+    }, 1200);
+}
+for(var i=0; i<introText.length; i++){
+    introText[i].style.height = (introTextSpan[i].getBoundingClientRect().height) + 'px';
 }
 
 window.addEventListener('scroll', function(){
@@ -95,8 +123,9 @@ window.addEventListener('scroll', function(){
     }
 
     // 섹션03
+    const circle = document.querySelector('.float');
+    const circle_exposure = document.querySelector('.line-circle-exposure');
     var circleValue = (windowY / 100) - 16;
-    var test01 = document.documentElement.scrollHeight - section03.offsetHeight;
     // +500은 섹션02 넘어갈 때 height 값을 100vh로 설정하는 sticky 클래스 때문인지 스크롤 동작이 제대로 안 돼서 추가함
     if(windowY >= section03.offsetTop + 500){
         circle.classList.add('sticky'); // sticky 클래스 추가
@@ -126,19 +155,33 @@ window.addEventListener('scroll', function(){
     }
 
     // 섹션06
-    const section06 = document.querySelector('.section06');
     const sc06_li = document.querySelectorAll('.section06 ul li');
+    const workImgSlide = document.querySelectorAll('.work-img-slide');
     var iBox = 0;
     for(var i=0; i<sc06_li.length; i++){
         sc06_li[i].style.opacity = '0.1';
-
-        //var sc06_li_offTop = sc06_li[i].offsetTop - 600;
+        workImgSlide[i].style.display = 'none';
         var opacityValue = 0.2 + (window.scrollY / document.documentElement.scrollHeight); 
 
         if(sc06_li[i].getBoundingClientRect().top < window.innerHeight / 2){ // 화면 중앙 탐지
             sc06_li[i].style.opacity = opacityValue;
+            workImgSlide[i].style.display = 'block';
         }else{
             sc06_li[i].style.opacity = '0.1';
+            workImgSlide[i].style.display = 'none';
         }
+    }
+
+    const bgCircle = document.querySelector('.bg-circle span');
+    bgCircle.style.width = '10vw';
+    bgCircle.style.height = '10vw';
+
+    const sc07ScrollTxt = document.querySelector('.section07 .txt-wrap h1');
+    if(window.scrollY >= section07.offsetTop + 300){
+        bgCircle.style.width = `${(window.scrollY / header.offsetHeight + footer.offsetHeight) / 3}vw`;
+        bgCircle.style.height = `${(window.scrollY / header.offsetHeight + footer.offsetHeight) / 3}vw`;
+
+        var lastValue = -window.scrollY / header.offsetHeight;
+        sc07ScrollTxt.style.transform = `translate3d(${lastValue + 387}vw, 0, 0) translate(-50%, -50%)`
     }
 });
